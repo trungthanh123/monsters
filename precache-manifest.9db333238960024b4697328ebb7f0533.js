c32e9e74db8ed711bea902e4d3e0b83e42e2b804
Trung Thanh <hoangtrungthanh1405@gmail.com>
2019-08-02 15:57:56 +0700

    Updates

---
+++ precache-manifest.9db333238960024b4697328ebb7f0533.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60068b66d470f36506f4282bde9fff1",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "15f27029d7870d7b1bbd",
+    "url": "/monsters/static/css/main.44e4a599.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "15f27029d7870d7b1bbd",
+    "url": "/monsters/static/js/main.81bffe67.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
